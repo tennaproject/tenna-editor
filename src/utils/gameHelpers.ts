c3e1eee5e5c7007f4dffa090fdc06b8e81861724
Jakub Jeżewski <jakub@jezewski.net>
2025-07-04 20:52:34 +0200

    feat: new data helpers

--- src/utils/gameHelpers.ts
+++ src/utils/gameHelpers.ts
@@ -1,5 +1,15 @@
-import { CONSUMABLES, CONSUMABLES_META } from '../data/consumables';
-import { FLAGS_META, FLAGS } from '../data/flags';
+import {
+  ARMORS,
+  ARMORS_META,
+  CONSUMABLES,
+  CONSUMABLES_META,
+  FLAGS,
+  FLAGS_META,
+  KEY_ITEMS,
+  KEY_ITEMS_META,
+  WEAPONS,
+  WEAPONS_META,
+} from '../data';
 import { createEntityHelpers } from './entityHelpers';
 
 export const flagHelpers = createEntityHelpers(FLAGS, FLAGS_META);
@@ -7,3 +17,6 @@ export const consumableHelpers = createEntityHelpers(
   CONSUMABLES,
   CONSUMABLES_META,
 );
+export const keyItemHelpers = createEntityHelpers(KEY_ITEMS, KEY_ITEMS_META);
+export const weaponHelpers = createEntityHelpers(WEAPONS, WEAPONS_META);
+export const armorHelpers = createEntityHelpers(ARMORS, ARMORS_META);
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,11 @@
+export { createEntityHelpers } from './entityHelpers';
+export {
+  flagHelpers,
+  consumableHelpers,
+  keyItemHelpers,
+  weaponHelpers,
+  armorHelpers,
+} from './gameHelpers';
 export { LineCursor } from './lineCursor';
-export { parseSaveFile } from './saveFileParser';
+export { SAVE_META, SUPPORTED_FORMATS, parseSaveFile } from './saveFileParser';
 export { serializeSaveFile } from './saveFileSerializer';
