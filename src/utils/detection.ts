66c4e0682bc746575f85ca02a0f525bd898e54c8
Jakub Jeżewski <jakub@jezewski.net>
2025-07-09 21:12:58 +0200

    fix(detection): remove debug log for roomPrefix

--- src/utils/detection.ts
+++ src/utils/detection.ts
@@ -17,7 +17,6 @@ export function detectChapter(save: DeltaruneSave): ChapterDetectionResult {
   }
 
   const roomPrefix = Number(String(save.room)[0]) as ChapterIndex;
-  console.log(roomPrefix);
   if (roomPrefix >= 1 && roomPrefix <= 4) {
     return {
       chapter: roomPrefix,
