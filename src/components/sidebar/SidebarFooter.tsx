b035531889485dace0523bae3c12816e49294b33
Jakub Jeżewski <jakub@jezewski.net>
2025-07-31 19:59:49 +0200

    Update SidebarFooter to simplify component and add trademark info

--- src/components/sidebar/SidebarFooter.tsx
+++ src/components/sidebar/SidebarFooter.tsx
@@ -1,10 +1,9 @@
 import { Button } from '../Button';
 export const SidebarFooter = () => {
   return (
-    <div className="p-3 bg-base select-none ">
-      <div className="text-xs text-subtle text-center">
-        <p>NOT AFFILIATED WITH TOBY FOX OR 8-4 LTD.</p>
-      </div>
-    </div>
+    <footer className="p-3 bg-base select-none text-center text-xs text-subtle">
+      <p>NOT AFFILIATED WITH TOBY FOX OR 8-4 LTD</p>
+      <p>DELTARUNE™ IS A REGISTERED TRADEMARK OF ROYAL SCIENCES, LLC</p>
+    </footer>
   );
 };
