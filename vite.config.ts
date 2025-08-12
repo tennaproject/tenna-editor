2dcb88d78e01ad007a444cd7b1f6039eba46667e
Jakub Jeżewski <jakub@jezewski.net>
2025-08-12 11:02:00 +0200

    Add svgr for loading icons

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,23 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tailwindcss from '@tailwindcss/vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
-  plugins: [react(), tailwindcss(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    tsconfigPaths(),
+    svgr({
+      svgrOptions: {
+        exportType: 'default',
+        ref: true,
+        svgo: false,
+        titleProp: true,
+      },
+      include: '**/*.svg',
+    }),
+  ],
   server: {
     host: '127.0.0.1',
     port: 4500,
