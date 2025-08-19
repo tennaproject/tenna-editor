02da0c4acd310a9fd5d584c0e71d789a696adc34
Jakub Jeżewski <jakub@jezewski.net>
2025-08-19 23:25:06 +0200

    Simplify Eslint config
    That last one didn't seem to work at all. It looks like this will work for now.

--- eslint.config.js
+++ eslint.config.js
@@ -1,59 +1,11 @@
-import globals from 'globals';
-import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
+// @ts-check
+
+import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
-
-import eslintPluginReact from 'eslint-plugin-react';
-import eslintPluginJsxA11y from 'eslint-plugin-jsx-a11y';
-
-import googleConfig from 'eslint-config-google';
 import prettierConfig from 'eslint-config-prettier';
 
-export default tseslint.config([
-  {
-    ignores: ['dist'],
-  },
-
-  {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      googleConfig,
-      ...tseslint.configs.strictTypeChecked,
-      ...tseslint.configs.stylisticTypeChecked,
-      prettierConfig,
-    ],
-
-    plugins: {
-      react: eslintPluginReact,
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-      'jsx-a11y': eslintPluginJsxA11y,
-    },
-
-    languageOptions: {
-      parser: tseslint.parser,
-      parserOptions: {
-        sourceType: 'module',
-        project: ['./tsconfig.node.json', './tsconfig.app.json'],
-        tsconfigRootDir: import.meta.dirname,
-        ecmaFeatures: {
-          jsx: true,
-        },
-      },
-      globals: {
-        ...globals.browser,
-      },
-    },
-
-    settings: {
-      react: {
-        version: 'detect',
-      },
-    },
-
-    rules: {
-      'valid-jsdoc': 'off',
-      'require-jsdoc': 'off',
-    },
-  },
-]);
+export default tseslint.config(
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  prettierConfig,
+);
