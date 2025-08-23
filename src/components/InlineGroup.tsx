f2251ac1a81c5e6eb14b2fc0e9381e071dd6c4cf
Jakub Jeżewski <jakub@jezewski.net>
2025-08-23 16:02:56 +0200

    Add custom classes to InlineGroup component

--- src/components/InlineGroup.tsx
+++ src/components/InlineGroup.tsx
@@ -1,9 +1,15 @@
+import { mergeClass } from '@utils';
 import type { ReactNode } from 'react';
 
 interface InlineGroupProps {
   children?: ReactNode;
+  className?: string;
 }
 
-export function InlineGroup({ children }: InlineGroupProps) {
-  return <div className="flex items-center gap-2">{children}</div>;
+export function InlineGroup({ children, className = '' }: InlineGroupProps) {
+  return (
+    <div className={mergeClass('flex items-center gap-2', className)}>
+      {children}
+    </div>
+  );
 }
