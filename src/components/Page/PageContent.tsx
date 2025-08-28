77d05394ab186d71c335030c75f4e75909996bb8
Jakub Jeżewski <jakub@jezewski.net>
2025-08-28 17:15:17 +0200

    Tweak scrollbar induced left padding

--- src/components/Page/PageContent.tsx
+++ src/components/Page/PageContent.tsx
@@ -6,7 +6,7 @@ interface PageContentProps {
 export function PageContent({ children }: PageContentProps) {
   return (
     <div
-      className="flex-1 min-h-0 flex flex-col select-none overflow-y-auto pl-[10px]"
+      className="flex-1 min-h-0 flex flex-col select-none overflow-y-auto lg:pl-[10px]"
       style={{ scrollbarGutter: 'stable ' }}
     >
       {children}
