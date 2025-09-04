8dfe799ba621a6712a99d3fffa465c2fbef08143
Jakub Jeżewski <jakub@jezewski.net>
2025-09-04 19:46:18 +0200

    Refine about's overview

--- src/pages/About/Overview.tsx
+++ src/pages/About/Overview.tsx
@@ -2,11 +2,11 @@ import { Section, Card, Heading } from '@components';
 
 export function AboutOverview() {
   return (
-    <div className="page">
-      <Section>
-        <Card className="space-y-4 p-6">
+    <article className="page">
+      <Section id="legal">
+        <Card className="flex flex-col gap-3 p-6">
           <Heading level={3}>Legal Info</Heading>
-          <div className="space-y-2 text-text-2">
+          <div className="flex flex-col text-text-2">
             <p>This is fan made project.</p>
             <p>
               This project is not affiliated with, endorsed by, or in any way
@@ -17,14 +17,41 @@ export function AboutOverview() {
         </Card>
       </Section>
 
-      <Section>
-        <Card className="space-y-4 p-6">
+      <Section id="privacy">
+        <Card className="flex flex-col gap-3 p-6">
+          <Heading level={3}>Privacy</Heading>
+          <div className="flex flex-col text-text-2 gap-2">
+            <p>
+              We don’t collect or store anything about you. All data is
+              processed on-device and never sent anywhere.
+            </p>
+            <div>
+              <p>
+                Tenna Editor is hosted on Cloudflare, which may collect some of
+                your personal data.
+              </p>
+              <a
+                href="https://www.cloudflare.com/trust-hub/privacy-and-data-protection"
+                className="text-red hover:text-red-hover underline"
+                target="_blank"
+              >
+                Click here to read Cloudflare's privacy policy and GDPR/HIPAA
+                compliance info.
+              </a>
+            </div>
+          </div>
+        </Card>
+      </Section>
+
+      <Section id="source">
+        <Card className="flex flex-col gap-3 p-6">
           <Heading level={3}>Source Code</Heading>
-          <p className="mb-2 text-text-2">
-            The source code for Tenna Editor is available on{' '}
+          <p className="text-text-2">
+            The source code of Tenna Editor is available on{' '}
             <a
               href="https://github.com/tennaproject/tenna-editor"
-              className="text-red hover:text-red-hover underline font-bold"
+              className="text-red hover:text-red-hover underline "
+              target="_blank"
             >
               GitHub
             </a>
@@ -32,16 +59,17 @@ export function AboutOverview() {
         </Card>
       </Section>
 
-      <Section>
-        <Card className="space-y-4 p-6">
+      <Section id="build">
+        <Card className="flex flex-col gap-3 p-6">
           <Heading level={3}>Build Info</Heading>
           <div className="text-text-2">
             <p>ID: {__COMMIT_HASH__}</p>
             <p>Version: {__VERSION__}</p>
             <p>Type: {__BRANCH__}</p>
+            <p>Timestamp: {__BUILD_TIMESTAMP__}</p>
           </div>
         </Card>
       </Section>
-    </div>
+    </article>
   );
 }
--- src/types/globals.d.ts
+++ src/types/globals.d.ts
@@ -1,4 +1,4 @@
 declare const __VERSION__: string;
 declare const __COMMIT_HASH__: string;
 declare const __BRANCH__: string;
-declare const __BUILD_TIMESTAMP__: Date;
+declare const __BUILD_TIMESTAMP__: string;
