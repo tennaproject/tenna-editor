c9d3123780f8a59e16e1008b51230134c1f055cf
Jakub Jeżewski <jakub@jezewski.net>
2026-01-15 13:45:06 +0100

    Fix package-lock.json disaster

--- scripts/release.sh
+++ scripts/release.sh
@@ -7,10 +7,9 @@ if [ -z "$version" ]; then
   exit 1
 fi
 
-sed -i "s/\"version\": \".*\"/\"version\": \"$version\"/" package.json
-sed -i "s/\"version\": \".*\"/\"version\": \"$version\"/" package-lock.json
+jq '.version = "'$version'"' package.json > package.json.tmp && mv package.json.tmp package.json
+jq '.version = "'$version'" | .packages[""].version = "'$version'"' package-lock.json > package-lock.json.tmp && mv package-lock.json.tmp package-lock.json
 
-git add package.json
-git add package-lock.json
+git add package.json package-lock.json
 git commit -m "Bump version to $version"
 git tag -a "v$version" -m "Release $version"
