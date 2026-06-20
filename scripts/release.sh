2c9392e9449dcd85ce97a7701b10ead9d1ea08d3
Jakub Jeżewski <jakub@jezewski.net>
2026-06-20 12:13:20 +0200

    Update changelog for 0.8.0-beta

--- scripts/release.sh
+++ scripts/release.sh
@@ -11,5 +11,7 @@ jq '.version = "'$version'"' package.json > package.json.tmp && mv package.json.
 jq '.version = "'$version'" | .packages[""].version = "'$version'"' package-lock.json > package-lock.json.tmp && mv package-lock.json.tmp package-lock.json
 
 git add package.json package-lock.json
-git commit -m "Bump version to $version"
-git tag -a "v$version" -m "Release $version"
+if ! git diff --cached --quiet package.json package-lock.json; then
+  git commit -m "Bump version to $version"
+fi
+git tag -f -a "v$version" -m "Release $version"
