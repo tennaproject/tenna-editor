188da1d3fe744dfac6e7fbb71577b26a25bb59d2
john <johnvvvvvvvvvvvv@proton.me>
2026-09-18 17:16:56 +0100

    Modify

--- src/data/contributors.ts
+++ src/data/contributors.ts
@@ -20,8 +20,6 @@ export const CONTRIBUTORS: Contributor[] = [
   },
   {
     displayName: 'cpu-99999999999',
-    login: 'cpu-99999999999',
-    url: 'https://github.com/cpu-99999999999',
   },
   {
     displayName: 'jah-yee',
